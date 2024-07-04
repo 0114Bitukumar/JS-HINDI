b3e83021485fffa489f37e6a99767e86a5723ab8
Bitu Kumar <144420067+0114Bitukumar@users.noreply.github.com>
2024-07-04 12:36:23 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsoroperations.js
@@ -0,0 +1,133 @@
+//"33" = 33
+//"33abc" = NaN
+//true = 1
+//false = 0
+
+//Boolean conversion 
+
+//1 => true
+//0 => false
+//"Bitu" => true
+//"" => false
+
+// const score = '33';
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any String convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+
+// const score = '33abc';
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any String convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);    // "33abc" ye value number me convert to ho ja rha hai
+// console.log( valueInNumber);          //but eska agar value check kiya jaye to ye NaN hai yani Not a Number. 
+
+
+// const score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any value convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);  
+
+
+
+// const score = "Bitu Kumar";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any value convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);  
+
+
+
+// const score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any value convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);  
+
+
+
+// const score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any value convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);  
+
+
+
+// const score = false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // if any value convert to number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);  
+
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);      // true
+
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);      // false
+
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);      // false
+
+
+
+// let isLoggedIn = "Bitu"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);      // true
+
+
+// let isLoggedIn = undefined
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);      // false
+
+
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
